76ce8ca4c434d899cf3f6e74fd142d6c0493487f
unknown <ranimhanna@hotmail.com>
2022-12-16 22:52:35 +0000

    Adds new routes

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/**
+ * Contains all routes 
  */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+"use strict";
+
+import m from "mithril";
+
+// Navigation
+import { layout } from "./views/layout.js";
+
+// view pages
+import { map } from "./views/map.js";
+import { history } from "./views/history.js";
+import { login } from "./views/login.js";
+import { payment } from "./views/payment.js";
+import { register } from "./views/register.js";
+import { settings } from "./views/settings.js";
+
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+function onDeviceReady() {
+
+    m.route(document.body, "/", {
+        "/": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/"
+                }, m(map));
+            }
+        },
+
+ 
+        "/payment": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/payment"
+                }, m(payment));
+            }
+        },
+
+        "/history": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/history"
+                }, m(history));
+            }
+        },
+
+        "/settings": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/settings"
+                }, m(settings));
+            }
+        },
+
+
+        "/register": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/register"
+                }, m(register));
+            }
+        },
+
+        "/login": {
+            render: function() {
+                return m(layout, {
+                    nav: "#!/login"
+                }, m(login));
+            }
+        }
+    });
 }
