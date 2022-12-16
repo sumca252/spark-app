4a1e96b1032af7be794e3202f500ae379bea964a
unknown <ranimhanna@hotmail.com>
2022-12-16 22:50:17 +0000

    Adds models files

---
+++ www/js/models/auth.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+*/
+
+
+"use strict";
+
+
+import m from 'mithril';
+
+let auth = {
+    url: "",
+    token: "",
+
+
+    login: function() {
+
+    },
+
+
+    register: function() {
+
+    }
+}
+
+
+export { auth };
---
+++ www/js/models/stations.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * 
+ * 
+*/
+
+
+"use strict";
+
+
+import m from 'mithril';
+
+let stations = {
+    url: "",
+
+    allStations: [],
+    station: [],
+
+
+    loadStations: function() {
+        // get all parking places/chargning stations
+        // adds stations data to allStations list
+    },
+
+    getSpecStation: function() {
+        // get a specific parking or charging station
+    },
+}
+
+
+export { stations };
